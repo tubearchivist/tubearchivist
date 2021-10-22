05b25d004ecbb88d62a30019cf82fb3fc7f52c2c
simon <simobilleter@gmail.com>
2021-10-22 18:23:06 +0700

    fix linting

--- tubearchivist/home/views.py
+++ tubearchivist/home/views.py
@@ -161,7 +161,7 @@ class LoginView(View):
         """handle login post request"""
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
-            next_url = request.POST.get('next') or "home"
+            next_url = request.POST.get("next") or "home"
             print(f"next url: {next_url}")
             user = form.get_user()
             login(request, user)
