8255fe9e55500cbbb58b3f55cdf9ab9dd5c6ca4b
simon <simobilleter@gmail.com>
2021-10-28 16:17:47 +0700

    validate expected env vars before starting

--- run.sh
+++ run.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 # startup script inside the container for tubearchivist
 
+# check environment
 if [[ -z "$ELASTIC_USER" ]]; then
     export ELASTIC_USER=elastic
 fi
 
+ENV_VARS=("TA_USERNAME" "TA_PASSWORD" "ELASTIC_PASSWORD" "ELASTIC_USER")
+for each in "${ENV_VARS[@]}"; do
+    if ! [[ -v $each ]]; then
+        echo "missing environment variable $each"
+        exit 1
+    fi
+done
+
+# wait for elasticsearch
 counter=0
 until curl -u "$ELASTIC_USER":"$ELASTIC_PASSWORD" "$ES_URL" -fs; do
     echo "waiting for elastic search to start"
@@ -17,6 +27,7 @@ until curl -u "$ELASTIC_USER":"$ELASTIC_PASSWORD" "$ES_URL" -fs; do
     sleep 10
 done
 
+# start python application
 python manage.py makemigrations
 python manage.py migrate
 export DJANGO_SUPERUSER_PASSWORD=$TA_PASSWORD && \
