3cc5db7ad1076280fb340157281f52a010266e85
simon <simobilleter@gmail.com>
2021-12-02 14:44:37 +0700

    use TZ environ for scheduler

--- tubearchivist/config/settings.py
+++ tubearchivist/config/settings.py
@@ -116,7 +116,7 @@ AUTH_USER_MODEL = "home.Account"
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
 LANGUAGE_CODE = "en-us"
-TIME_ZONE = "UTC"
+TIME_ZONE = environ.get("TZ") or "UTC"
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
