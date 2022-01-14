46ba988a0ed33fccd9b987fe62da97975c683952
n8detar <n8detar@gmail.com>
2022-01-13 21:39:31 -0800

    Cast video at current playback position

--- tubearchivist/static/cast-videos.js
+++ tubearchivist/static/cast-videos.js
@@ -1,6 +1,6 @@
 function initializeCastApi() {
     cast.framework.CastContext.getInstance().setOptions({
-        receiverApplicationId: chrome.cast.media.DEFAULT_MEDIA_RECEIVER_APP_ID,  // Use built in reciver app on cast device
+        receiverApplicationId: chrome.cast.media.DEFAULT_MEDIA_RECEIVER_APP_ID, // Use built in reciver app on cast device, see https://developers.google.com/cast/docs/styled_receiver if you want to be able to add a theme, splash screen or watermark. Has a $5 one time fee.
         autoJoinPolicy: chrome.cast.AutoJoinPolicy.ORIGIN_SCOPED
     });
 
@@ -8,15 +8,16 @@ function initializeCastApi() {
     var playerController = new cast.framework.RemotePlayerController(player);
 
     // Add event listerner to check if a connection to a cast device is initiated
-    playerController.addEventListener(cast.framework.RemotePlayerEventType.IS_CONNECTED_CHANGED, 
-        function () {
-            castConnectionChange(player);
+    playerController.addEventListener(
+        cast.framework.RemotePlayerEventType.IS_CONNECTED_CHANGED, function() { 
+            castConnectionChange(player) 
         }
     );
-};
+}
+
 
 function castConnectionChange(player) {
-    // If cast connection is intitialized start cast
+    // If cast connection is initialized start cast
     if (player.isConnected) {
         // console.log("Cast Connected.");
         castStart();
@@ -30,20 +31,22 @@ function castStart() {
     
     // Check if there is already media playing on the cast target to prevent recasting on page reload or switching to another video page
     if (!castSession.getMediaSession()) { 
-        contentId = document.getElementById("video-item").src; // Get URL from the video item
-        contentTitle = document.getElementById('video-title').innerHTML; // Get Video title
+        contentId = document.getElementById("video-item").src; // Get video URL
+        contentTitle = document.getElementById('video-title').innerHTML; // Get video title
         contentImage = document.getElementById("video-item").poster; // Get video thumbnail URL
         contentType = 'video/mp4'; // Set content type, only videos right now so it is hard coded
+        contentCurrentTime = document.getElementById("video-item").currentTime; // Get video's current position
 
-        mediaInfo = new chrome.cast.media.MediaInfo(contentId, contentType); // Create mediainfo var that contains url and content type
-        mediaInfo.streamType = chrome.cast.media.StreamType.BUFFERED; // Set type of stream, in this case a normal video rather than a Livestream. I don't think this needs to be set sample code had it.
-        mediaInfo.metadata = new chrome.cast.media.TvShowMediaMetadata(); // Create metadata var and add it to mediaInfo
+        mediaInfo = new chrome.cast.media.MediaInfo(contentId, contentType); // Create MediaInfo var that contains url and content type
+        // mediaInfo.streamType = chrome.cast.media.StreamType.BUFFERED; // Set type of stream, BUFFERED, LIVE, OTHER
+        mediaInfo.metadata = new chrome.cast.media.GenericMediaMetadata(); // Create metadata var and add it to MediaInfo
         mediaInfo.metadata.title = contentTitle; // Set the video title
-        mediaInfo.metadata.images = [{
-            'url': contentImage // Set the thumbnail. IDK if this is working correctly, something to check later
-        }];
+        mediaInfo.metadata.images = [new chrome.cast.Image(contentImage)]; // Set the video thumbnail
 
-        var request = new chrome.cast.media.LoadRequest(mediaInfo); // Create request
+        var request = new chrome.cast.media.LoadRequest(mediaInfo); // Create request with the previously set MediaInfo. 
+        // request.queueData = new chrome.cast.media.QueueData(); // See https://developers.google.com/cast/docs/reference/web_sender/chrome.cast.media.QueueData for playlist support.
+        request.currentTime = shiftCurrentTime(contentCurrentTime); // Set video start position based on the browser video position
+        // request.autoplay = false; // Set content to auto play, true by default
         castSession.loadMedia(request).then(
             function() { 
                 castSuccessful();
@@ -53,6 +56,14 @@ function castStart() {
             }
         ); // Send request to cast device
     }   
+}
+
+function shiftCurrentTime(contentCurrentTime) { // Shift media back 3 seconds to prevent missing some of the content
+    if (contentCurrentTime > 5) {
+        return(contentCurrentTime - 3);
+    } else {
+        return(0);
+    }
 }
 
 function castSuccessful() {
@@ -68,4 +79,4 @@ window['__onGCastApiAvailable'] = function(isAvailable) {
     if (isAvailable) {
         initializeCastApi();
     }
-};
+}
