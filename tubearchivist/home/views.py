b1a7a6a148f07c68daeb18015971306c4acdab69
simon <simobilleter@gmail.com>
2022-04-07 22:30:20 +0700

    use cleaned_data for config form parser

--- tubearchivist/home/views.py
+++ tubearchivist/home/views.py
@@ -8,7 +8,6 @@ import json
 import urllib.parse
 from time import sleep
 
-from django import forms
 from django.conf import settings
 from django.contrib.auth import login
 from django.contrib.auth.forms import AuthenticationForm
@@ -802,23 +801,25 @@ class SettingsView(View):
     @staticmethod
     def post(request):
         """handle form post to update settings"""
+        user_form = UserSettingsForm(request.POST)
+        if user_form.is_valid():
+            user_form_post = user_form.cleaned_data
+            if any(user_form_post.values()):
+                AppConfig().set_user_config(user_form_post, request.user.id)
 
-        form_response = forms.Form(request.POST)
-        if form_response.is_valid():
-            form_post = dict(request.POST)
-            print(form_post)
-            del form_post["csrfmiddlewaretoken"]
-            config_handler = AppConfig()
-            if "application-settings" in form_post:
-                del form_post["application-settings"]
-                config_handler.update_config(form_post)
-            elif "user-settings" in form_post:
-                del form_post["user-settings"]
-                config_handler.set_user_config(form_post, request.user.id)
-            elif "scheduler-settings" in form_post:
-                del form_post["scheduler-settings"]
-                print(form_post)
-                ScheduleBuilder().update_schedule_conf(form_post)
+        app_form = ApplicationSettingsForm(request.POST)
+        if app_form.is_valid():
+            app_form_post = app_form.cleaned_data
+            if app_form_post:
+                print(app_form_post)
+                AppConfig().update_config(app_form_post)
+
+        scheduler_form = SchedulerSettingsForm(request.POST)
+        if scheduler_form.is_valid():
+            scheduler_form_post = scheduler_form.cleaned_data
+            if any(scheduler_form_post.values()):
+                print(scheduler_form_post)
+                ScheduleBuilder().update_schedule_conf(scheduler_form_post)
 
         sleep(1)
         return redirect("settings", permanent=True)
