eccad8968bd120a02f934fa90656d3d322ab34b9
simon <simobilleter@gmail.com>
2023-01-04 11:53:46 +0700

    add unknown field to VideoTypeEnum

--- tubearchivist/home/src/index/video_constants.py
+++ tubearchivist/home/src/index/video_constants.py
@@ -1,7 +1,12 @@
+"""video constants"""
+
 import enum
 
 
 class VideoTypeEnum(enum.Enum):
+    """all vid_type fields"""
+
     VIDEO = "video"
     LIVE = "live"
     SHORT = "short"
+    UNKNOWN = "unknown"
