18ba8086642018755c2d24ee04d3b790c1df6500
Simon <simobilleter@gmail.com>
2024-01-15 12:06:03 +0700

    bump TA_VERSION unstable

--- tubearchivist/config/settings.py
+++ tubearchivist/config/settings.py
@@ -269,4 +269,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 
 # TA application settings
 TA_UPSTREAM = "https://github.com/tubearchivist/tubearchivist"
-TA_VERSION = "v0.4.5"
+TA_VERSION = "v0.4.6-unstable"
