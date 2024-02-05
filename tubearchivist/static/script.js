8778546577d8e87df7f1317436d9e75202a69fd0
Simon <simobilleter@gmail.com>
2024-02-05 21:55:05 +0100

    remove autoplay, disable video progress less than 10s

--- tubearchivist/static/script.js
+++ tubearchivist/static/script.js
@@ -451,7 +451,7 @@ function createPlayer(button) {
   }
   let videoName = videoData.data.title;
 
-  let videoTag = createVideoTag(videoData, videoProgress);
+  let videoTag = createVideoTag(videoData, videoProgress, true);
 
   let playlist = '';
   let videoPlaylists = videoData.data.playlist; // Array of playlists the video is in
@@ -529,7 +529,7 @@ function insertVideoTag(videoData, videoProgress) {
 }
 
 // Generates a video tag with subtitles when passed videoData and videoProgress.
-function createVideoTag(videoData, videoProgress) {
+function createVideoTag(videoData, videoProgress, autoplay = false) {
   let videoId = videoData.data.youtube_id;
   let videoUrl = videoData.data.media_url;
   let videoThumbUrl = videoData.data.vid_thumb_url;
@@ -546,7 +546,9 @@ function createVideoTag(videoData, videoProgress) {
   }
 
   let videoTag = `
-    <video poster="${videoThumbUrl}" onvolumechange="onVolumeChange(this)" onloadstart="this.volume=getPlayerVolume()" ontimeupdate="onVideoProgress()" onpause="onVideoPause()" onended="onVideoEnded()" controls autoplay width="100%" playsinline id="video-item">
+    <video poster="${videoThumbUrl}" onvolumechange="onVolumeChange(this)" onloadstart="this.volume=getPlayerVolume()" ontimeupdate="onVideoProgress()" onpause="onVideoPause()" onended="onVideoEnded()" ${
+    autoplay ? 'autoplay' : ''
+  } controls width="100%" playsinline id="video-item">
         <source src="${videoUrl}#t=${videoProgress}" type="video/mp4" id="video-source" videoid="${videoId}">
         ${subtitles}
     </video>
@@ -650,6 +652,7 @@ function onVideoProgress() {
       }
     }
   }
+  if (currentTime < 10) return;
   if ((currentTime % 10).toFixed(1) <= 0.2) {
     // Check progress every 10 seconds or else progress is checked a few times a second
     postVideoProgress(videoId, currentTime);
@@ -701,6 +704,7 @@ function watchedThreshold(currentTime, duration) {
 function onVideoPause() {
   let videoId = getVideoPlayerVideoId();
   let currentTime = getVideoPlayerCurrentTime();
+  if (currentTime < 10) return;
   postVideoProgress(videoId, currentTime);
 }
 
