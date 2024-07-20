2afc2a6b5adecb345279ad7191c707076ee8ce0f
MerlinScheurer <merlin.scheurer@Yahoo.de>
2024-07-20 11:17:30 +0200

    Add redirect to login after logout

--- tubearchivist-frontend/src/pages/Logout.tsx
+++ tubearchivist-frontend/src/pages/Logout.tsx
@@ -1,7 +1,15 @@
+import { useNavigate } from 'react-router-dom';
 import importColours, { ColourConstant, ColourVariants } from '../configuration/colours/getColours';
+import Routes from '../configuration/routes/RouteList';
+import { useEffect } from 'react';
 
 const Logout = () => {
   importColours(ColourConstant.Dark as ColourVariants);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate(Routes.Login);
+  }, []);
 
   return <>Logout</>;
 };
