b864813a7b687ec0a1fd8f878c09bddb533e5034
Simon <simobilleter@gmail.com>
2024-08-03 23:37:23 +0200

    fix tests

--- backend/common/tests/conftest.py
+++ backend/common/tests/conftest.py
@@ -8,4 +8,4 @@ import pytest
 @pytest.fixture(scope="session", autouse=True)
 def change_test_dir(request):
     """change directory to project folder"""
-    os.chdir(request.config.rootdir / "tubearchivist")
+    os.chdir(request.config.rootdir / "backend")
