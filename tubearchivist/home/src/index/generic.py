28d89382103a80e2d534725af92e6b5cdcb224e6
Will Hughes <53810208+willhughes-au@users.noreply.github.com>
2024-08-25 15:31:19 +1000

    fix typo

--- tubearchivist/home/src/index/generic.py
+++ tubearchivist/home/src/index/generic.py
@@ -31,7 +31,7 @@ class YouTubeItem:
 
     def build_yt_url(self):
         """build youtube url"""
-        print(f"[build_yt_url] - self null? {self is None}; self.yt_base is None? {self.yt_base is None}; self.youtube_id is None? {self.youtube_id};")
+        print(f"[build_yt_url] - self null? {self is None}; self.yt_base is None? {self.yt_base is None}; self.youtube_id is None? {self.youtube_id is None};")
         return self.yt_base + self.youtube_id
 
     def get_from_youtube(self):
