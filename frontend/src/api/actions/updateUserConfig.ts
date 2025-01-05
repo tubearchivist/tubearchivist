f911ed9d1a6cf6bacab744247858d0b49755b68f
Simon <simobilleter@gmail.com>
2025-01-05 12:07:04 +0700

    home filter bar from config store

--- frontend/src/api/actions/updateUserConfig.ts
+++ frontend/src/api/actions/updateUserConfig.ts
@@ -18,22 +18,21 @@ export type UserMeType = {
 export type ColourVariants = 'dark.css' | 'light.css' | 'matrix.css' | 'midnight.css';
 
 export type UserConfigType = {
-  stylesheet?: ColourVariants;
-  page_size?: number;
-  sort_by?: SortByType;
-  sort_order?: SortOrderType;
-  view_style_home?: ViewLayoutType;
-  view_style_channel?: ViewLayoutType;
-  view_style_downloads?: ViewLayoutType;
-  view_style_playlist?: ViewLayoutType;
-  grid_items?: number;
-  hide_watched?: boolean;
-  show_ignored_only?: boolean;
-  show_subed_only?: boolean;
-  sponsorblock_id?: number;
+  stylesheet: ColourVariants;
+  page_size: number;
+  sort_by: SortByType;
+  sort_order: SortOrderType;
+  view_style_home: ViewLayoutType;
+  view_style_channel: ViewLayoutType;
+  view_style_downloads: ViewLayoutType;
+  view_style_playlist: ViewLayoutType;
+  grid_items: number;
+  hide_watched: boolean;
+  show_ignored_only: boolean;
+  show_subed_only: boolean;
 };
 
-const updateUserConfig = async (config: UserConfigType): Promise<UserConfigType> => {
+const updateUserConfig = async (config: Partial<UserConfigType>): Promise<UserConfigType> => {
   const apiUrl = getApiUrl();
   const csrfCookie = getCookie('csrftoken');
 
--- frontend/src/stores/UserConfigStore.ts
+++ frontend/src/stores/UserConfigStore.ts
@@ -4,7 +4,7 @@ import updateUserConfig, { UserMeType, UserConfigType } from '../api/actions/upd
 interface UserConfigState {
   userConfig: UserMeType;
   setUserConfig: (userConfig: UserMeType) => void;
-  setPartialConfig: (updates: UserConfigType) => void;
+  setPartialConfig: (userConfig: Partial<UserConfigType>) => void;
 }
 
 export const useUserConfigStore = create<UserConfigState>((set) => ({
@@ -17,12 +17,25 @@ export const useUserConfigStore = create<UserConfigState>((set) => ({
     groups: [],
     user_permissions: [],
     last_login: '',
-    config: {}
+    config: {
+      stylesheet: 'dark.css',
+      page_size: 12,
+      sort_by: 'published',
+      sort_order: 'desc',
+      view_style_home: 'grid',
+      view_style_channel: 'list',
+      view_style_downloads: 'list',
+      view_style_playlist: 'grid',
+      grid_items: 3,
+      hide_watched: false,
+      show_ignored_only: false,
+      show_subed_only: false,
+    }
   },
   setUserConfig: (userConfig) => set({ userConfig }),
 
-  setPartialConfig: async (updates: UserConfigType) => {
-    const userConfigResponse = await updateUserConfig(updates);
+  setPartialConfig: async (userConfig: Partial<UserConfigType>) => {
+    const userConfigResponse = await updateUserConfig(userConfig);
     set((state) => ({
       userConfig: state.userConfig ? { ...state.userConfig, config: userConfigResponse } : state.userConfig,
     }));
