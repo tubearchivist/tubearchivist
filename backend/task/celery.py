73bd3924885a278af3879988d84d03f48df71788
Simon <simobilleter@gmail.com>
2025-01-27 16:36:19 +0700

    parse unix connection string for redis socket

--- backend/task/celery.py
+++ backend/task/celery.py
@@ -5,12 +5,21 @@ import os
 from celery import Celery
 from common.src.env_settings import EnvironmentSettings
 
+
+def con_parser():
+    """allow for unix socket parsing"""
+    redis_con = EnvironmentSettings.REDIS_CON
+    if redis_con.startswith("unix://"):
+        redis_con = EnvironmentSettings.REDIS_CON.replace(
+            "unix://", "redis+socket://", 1
+        )
+
+    return redis_con
+
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 app = Celery(
-    "tasks",
-    broker=EnvironmentSettings.REDIS_CON,
-    backend=EnvironmentSettings.REDIS_CON,
-    result_extended=True,
+    "tasks", broker=con_parser(), backend=con_parser(), result_extended=True
 )
 app.config_from_object(
     "django.conf:settings", namespace=EnvironmentSettings.REDIS_NAME_SPACE
