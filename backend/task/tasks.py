73f7e8945cf91e01da7516e5372db3b4ce74d765
Simon <simobilleter@gmail.com>
2025-03-08 17:44:17 +0700

    fix spelling

--- backend/task/tasks.py
+++ backend/task/tasks.py
@@ -272,9 +272,9 @@ def thumbnail_check(self):
         return
 
     manager.init(self)
-    thumnail = ThumbValidator(task=self)
-    thumnail.validate()
-    thumnail.clean_up()
+    thumbnail = ThumbValidator(task=self)
+    thumbnail.validate()
+    thumbnail.clean_up()
 
 
 @shared_task(bind=True, name="resync_thumbs", base=BaseTask)
