c6329571ae4471ea21f528a5a2c8470e0e0a7bd0
Simon <simobilleter@gmail.com>
2025-03-25 09:10:52 +0100

    add unstable TA_VERSION

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -294,7 +294,7 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 
 # TA application settings
 TA_UPSTREAM = "https://github.com/tubearchivist/tubearchivist"
-TA_VERSION = "v0.5.0"
+TA_VERSION = "v0.5.1-unstable"
 
 # API
 REST_FRAMEWORK = {
