afb3582bcf89283482597b41d8ef72f2bed06da8
Simon <simobilleter@gmail.com>
2026-02-07 09:28:41 +0700

    bump TA_VERSION

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -221,7 +221,7 @@ CORS_EXPOSE_HEADERS = ["X-Start-Timestamp"]
 
 # TA application settings
 TA_UPSTREAM = "https://github.com/tubearchivist/tubearchivist"
-TA_VERSION = "v0.5.9-unstable"
+TA_VERSION = "v0.5.9"
 try:
     TA_START = RedisArchivist().get_message_str("STARTTIMESTAMP")
 except ValueError:
